7002077dc2eea8c8ade492edf6bf7c819401b235
Gabriel Sousa <g.sousa@docente.senai.br>
2026-08-07 19:16:11 -0300

    Atualizando com o conteúdo da aula 13

--- proj_fast/app.py
+++ proj_fast/app.py
@@ -1,8 +1,23 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from proj_fast.schemas import Message, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.post(
+        '/users',
+        status_code=HTTPStatus.CREATED,
+)
+def create_user(user: UserSchema):
+    ...
+
+@app.get(
+    '/',
+    status_code=HTTPStatus.OK,
+    response_model=Message,
+)
 def read_root():
     return {'message': 'Olá, Mundo!'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,9 @@ def test_ok_ola():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá, Mundo!'}
+
+def test_read_html(client):
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '<h1> Olá Mundo!</h1>'

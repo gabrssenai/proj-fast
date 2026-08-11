bb99274a0bfbf9ea7112332d279b385d952bef5b
Gabriel Sousa <g.sousa@docente.senai.br>
2026-08-10 21:24:16 -0300

    Atulalizando com Update

--- proj_fast/app.py
+++ proj_fast/app.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
+from fastapi import HTTPException
+
 from proj_fast.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
@@ -17,6 +19,24 @@ database: list[UserDB] = []
 def create_user(user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
     database.append(user_with_id)
+    return user_with_id
+
+
+@app.put(
+    '/users/{user_id}',
+    response_model=UserPublic,
+)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found',
+        )
+    user_with_id = UserDB(
+        **user.model_dump(),
+        id=user_id,
+    )
+    database[user_id - 1] = user_with_id
     return user_with_id
 
 

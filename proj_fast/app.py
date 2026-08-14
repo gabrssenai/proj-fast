fb504347209915ace3b48e9e98af84be8106085a
Gabriel Sousa <g.sousa@docente.senai.br>
2026-08-14 19:03:56 -0300

    Atualizando com persistencia

--- proj_fast/app.py
+++ proj_fast/app.py
@@ -10,6 +10,8 @@ from proj_fast.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 app = FastAPI()
 
 
+database: list[UserDB] = []
+
 @app.post(
     '/users/',
     status_code=HTTPStatus.CREATED,

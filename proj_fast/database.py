4de21edb29a56629a827d069eecacf72e2045b44
Gabriel Sousa <g.sousa@docente.senai.br>
2026-08-13 21:20:19 -0300

    Adicionando o script database

---
+++ proj_fast/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from proj_fast.models import table_registry
+
+
+engine = create_engine('sqlite:///database.db')
+
+table_registry.metadata.create_all(engine)
+
+#uv run python -c "import proj_fast.database"

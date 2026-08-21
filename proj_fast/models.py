f7a192b8fffafefc9accfef32656311a9cd8aaa8
Gabriel Sousa <g.sousa@docente.senai.br>
2026-08-20 21:23:52 -0300

    Adicionando tabela de interesses

--- proj_fast/models.py
+++ proj_fast/models.py
@@ -21,3 +21,26 @@ class User:
         init=False,
         server_default=func.now(),
     )
+
+@mapped_as_dataclass(table_registry)
+class Interest:
+    __tablename__ = "interests"
+
+    id: Mapped[int] = mapped_column(
+        init=False,
+        primary_key=True,
+    )
+    nome: Mapped[str]
+    email: Mapped[str]
+    cidade: Mapped[str]
+    veiculo_id: Mapped[int]
+    veiculo_modelo: Mapped[str]
+
+    status: Mapped[str] = mapped_column(
+        default="novo",
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        init=False,
+        server_default=func.now(),
+    )
